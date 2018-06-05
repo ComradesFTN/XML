bef9f26843d430a362583be45d449c775f7b4aa5
nesha95 <pijanic.nenad@gmail.com>
2018-06-05 15:33:55 +0200

    da

---
+++ xmlws/src/main/java/ftn/xmlws/XmlwsApplication.java
@@ -0,0 +1,12 @@
+package ftn.xmlws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlwsApplication.class, args);
+	}
+}
