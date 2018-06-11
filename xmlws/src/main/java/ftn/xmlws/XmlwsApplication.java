19ba0f087f3dc33220e5afb89e307177bd4f3abb
Mihailo M <mihailom@protonmail.com>
2018-06-11 20:20:59 +0200

    User services.

--- xmlws/src/main/java/ftn/xmlws/XmlwsApplication.java
+++ xmlws/src/main/java/ftn/xmlws/XmlwsApplication.java
@@ -1,14 +1,37 @@
 package ftn.xmlws;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.apache.cxf.transport.servlet.CXFServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+
+import ftn.xmlws.config.Config;
 
 @SpringBootApplication
-@ImportResource({ "classpath:cxf.xml" })
-public class XmlwsApplication {
+public class XmlwsApplication implements WebApplicationInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(XmlwsApplication.class, args);
+	}
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+		rootContext.register(Config.class);
+		servletContext.addListener(new ContextLoaderListener(rootContext));
+
+		AnnotationConfigWebApplicationContext webContext = new AnnotationConfigWebApplicationContext();
+		webContext.setParent(rootContext);
+
+		ServletRegistration.Dynamic cxfDispatcher = servletContext.addServlet("CXF dispatcher", CXFServlet.class);
+		cxfDispatcher.setLoadOnStartup(1);
+		cxfDispatcher.addMapping("/services/*");
+
 	}
 }
