06cdf2c399cfb8314fa03e8d81f0decbfec2d3f3
Mihailo M <mihailom@protonmail.com>
2018-06-17 16:41:16 +0200

    Service completed.

--- xmlws/src/main/java/ftn/xmlws/XmlwsApplication.java
+++ xmlws/src/main/java/ftn/xmlws/XmlwsApplication.java
@@ -2,13 +2,19 @@ package ftn.xmlws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class XmlwsApplication {	
+public class XmlwsApplication extends SpringBootServletInitializer {	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(XmlwsApplication.class, args);
 	}	
 	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(XmlwsApplication.class);
+    }
 	
 }
