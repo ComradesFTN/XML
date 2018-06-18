7cc0408a2087838f3c763db4d5a1f05dc9a79a84
GrYloX <goranrajic95@gmail.com>
2018-06-18 21:47:30 +0200

    commit

--- xmlws/src/main/java/ftn/xmlws/service/soap/AccomodationServiceSoap.java
+++ xmlws/src/main/java/ftn/xmlws/service/soap/AccomodationServiceSoap.java
@@ -3,10 +3,19 @@ package ftn.xmlws.service.soap;
 import javax.jws.WebService;
 
 import ftn.xmlws.domain.Accomodation;
+import ftn.xmlws.domain.AccomodationImage;
+import ftn.xmlws.domain.AccomodationType;
+import ftn.xmlws.domain.Category;
+import ftn.xmlws.domain.ExtraServices;
+import ftn.xmlws.domain.Term;
 
 @WebService
 public interface AccomodationServiceSoap {
 	
-	public Accomodation save(Accomodation acc);
-	
+	public Accomodation save(Accomodation acc);	
+	public AccomodationImage findImageById(Long id);
+	public AccomodationType findTypeById(Long id);
+	public Category findCategoryById(Long id);
+	public ExtraServices findExtraServicesById(Long id);
+	public Term findTermById(Long id);
 }
--- xmlws/src/main/java/ftn/xmlws/service/soap/AccomodationServiceSoapImpl.java
+++ xmlws/src/main/java/ftn/xmlws/service/soap/AccomodationServiceSoapImpl.java
@@ -6,6 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import ftn.xmlws.domain.Accomodation;
+import ftn.xmlws.domain.AccomodationImage;
+import ftn.xmlws.domain.AccomodationType;
+import ftn.xmlws.domain.Category;
+import ftn.xmlws.domain.ExtraServices;
+import ftn.xmlws.domain.Term;
 import ftn.xmlws.repository.AccomodationImageRepository;
 import ftn.xmlws.repository.AccomodationRepository;
 import ftn.xmlws.repository.AccomodationTypeRepository;
@@ -39,6 +44,32 @@ public class AccomodationServiceSoapImpl implements AccomodationServiceSoap {
 	public Accomodation save(Accomodation acc) {			
 		return accomodationRepository.save(acc);
 	}
+
+	@Override
+	public AccomodationImage findImageById(Long id) {
+		return accomodationImageRepository.findById(id).get();
+	}
+
+	@Override
+	public AccomodationType findTypeById(Long id) {
+		return accomodationTypeRepository.findById(id).get();
+	}
+
+	@Override
+	public Category findCategoryById(Long id) {
+		return categoryRepository.findById(id).get();
+	}
+
+	@Override
+	public ExtraServices findExtraServicesById(Long id) {
+		return extraServicesRepository.findById(id).get();
+	}
+
+	@Override
+	public Term findTermById(Long id) {
+		return termRepository.findById(id).get();
+	}
+	
 	
 	
 
